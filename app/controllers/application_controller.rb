f95bc3c5f17edb72b3cf5443a86e6185204b8014
Ryan Stocker <stocker.19@osu.edu>
2011-06-15 14:28:00 -0400

    Changed tabs to spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,88 +2,88 @@ class ApplicationController < ActionController::Base
 
   include SslRequirement
 
-	protect_from_forgery
+  protect_from_forgery
 
-	helper_method :current_user, :current_user_session
+  helper_method :current_user, :current_user_session
 
-	before_filter :require_user
+  before_filter :require_user
 
-	private
+  private
 
-	def authenticated?
-		request.env['employeeNumber'].present?
-	end
+  def authenticated?
+    request.env['employeeNumber'].present?
+  end
 
-	def shibboleth
-		{:emplid => request.env['employeeNumber'],
-		 :name_n => request.env['REMOTE_USER'].chomp("@osu.edu")}
-	end
+  def shibboleth
+    {:emplid => request.env['employeeNumber'],
+     :name_n => request.env['REMOTE_USER'].chomp("@osu.edu")}
+  end
 
-	def current_user_session
-		if session[:simulate_id].present?
+  def current_user_session
+    if session[:simulate_id].present?
       @current_user_session = UserSession.create_from_id(session[:simulate_id])
     elsif defined?(@current_user_session)
-			@current_user_session
-		elsif @current_user_session = UserSession.find
-			@current_user_session
-		elsif authenticated?
-			@current_user_session = UserSession.create_from_shibboleth(shibboleth)
-		end
-	end
+      @current_user_session
+    elsif @current_user_session = UserSession.find
+      @current_user_session
+    elsif authenticated?
+      @current_user_session = UserSession.create_from_shibboleth(shibboleth)
+    end
+  end
 
-	def current_user
-		return @current_user if defined?(@current_user)
-		@current_user = current_user_session && current_user_session.user
-	end
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
 
-	def require_user
-		unless current_user
-			if Rails.env.production?
-				base = request.protocol + request.host
-				requested_url = base + request.request_uri
-				redirect_to [base, '/Shibboleth.sso/Login?target=',
-					CGI.escape(requested_url)].join
-			else
-				redirect_to new_user_session_url, :notice => 'Login first, please.'
-			end
-		end
-	end
+  def require_user
+    unless current_user
+      if Rails.env.production?
+        base = request.protocol + request.host
+        requested_url = base + request.request_uri
+        redirect_to [base, '/Shibboleth.sso/Login?target=',
+          CGI.escape(requested_url)].join
+      else
+        redirect_to new_user_session_url, :notice => 'Login first, please.'
+      end
+    end
+  end
 
-	def admin_required
-		unless current_user.admin?
-			redirect_to root_url, :notice => "You must be an administrator to access this page"
-		end
-	end
+  def admin_required
+    unless current_user.admin?
+      redirect_to root_url, :notice => "You must be an administrator to access this page"
+    end
+  end
 
-	def advisor_required
-		if current_user.student? and current_user.switchable?
-			current_user.alternate_owner!
-		end
-		if current_user.advisor?
-			@advisor = current_user.owner
-		else
-			redirect_to root_url, :notice => "You must be an advisor to access this part of the system"
-		end
-	end
+  def advisor_required
+    if current_user.student? and current_user.switchable?
+      current_user.alternate_owner!
+    end
+    if current_user.advisor?
+      @advisor = current_user.owner
+    else
+      redirect_to root_url, :notice => "You must be an advisor to access this part of the system"
+    end
+  end
 
-	def student_required
-		if current_user.advisor? and current_user.switchable?
-			current_user.alternate_owner!
-		end
-		if current_user.student?
-			@student = current_user.owner
-			redirect_to student_disclaimer_url if !current_user.agreed_to_student_usage?
-		else
-			redirect_to root_url, :notice => "You must be a student to access this part of the system"
-		end
-	end
+  def student_required
+    if current_user.advisor? and current_user.switchable?
+      current_user.alternate_owner!
+    end
+    if current_user.student?
+      @student = current_user.owner
+      redirect_to student_disclaimer_url if !current_user.agreed_to_student_usage?
+    else
+      redirect_to root_url, :notice => "You must be a student to access this part of the system"
+    end
+  end
 
 
-	protected
+  protected
 
-	#require SSL only in production
-	def ssl_required?
-		Rails.env.production?
-	end
+  #require SSL only in production
+  def ssl_required?
+    Rails.env.production?
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,53 +1,53 @@
 class User < ActiveRecord::Base
-	include OhioStatePerson
+  include OhioStatePerson
 
-	belongs_to :owner, :polymorphic => true
-	has_many :authorizations
+  belongs_to :owner, :polymorphic => true
+  has_many :authorizations
 
-	acts_as_authentic do |c|
-		c.validate_password_field = false
-	end
+  acts_as_authentic do |c|
+    c.validate_password_field = false
+  end
 
-	def self.find_or_create_from_shibboleth(identity)
-		user = find_or_create_by_emplid(identity)
+  def self.find_or_create_from_shibboleth(identity)
+    user = find_or_create_by_emplid(identity)
 
-		# names change due to marriage, etc.
-		# update_attribute is a NOOP if not different
-		user.update_attribute(:name_n, identity[:name_n])
+    # names change due to marriage, etc.
+    # update_attribute is a NOOP if not different
+    user.update_attribute(:name_n, identity[:name_n])
 
-		user
-	end
+    user
+  end
 
-	def admin?
-		role == 'admin'
-	end
+  def admin?
+    role == 'admin'
+  end
 
-	def authorized?
-		admin? or !!Authorization.find_by_user_id(self.id)
-	end
+  def authorized?
+    admin? or !!Authorization.find_by_user_id(self.id)
+  end
 
-	def advisor?
-		find_owner if owner_type.nil?
-		owner_type == 'Advisor' and authorized?
-	end
+  def advisor?
+    find_owner if owner_type.nil?
+    owner_type == 'Advisor' and authorized?
+  end
 
-	def advisor
-		Advisor.find_by_emplid(self.emplid)
-	end
+  def advisor
+    Advisor.find_by_emplid(self.emplid)
+  end
 
-	def student?
-		find_owner if owner_type.nil?
-		owner_type == 'Student'
-	end
+  def student?
+    find_owner if owner_type.nil?
+    owner_type == 'Student'
+  end
 
-	def student
-		Student.find_by_emplid(self.emplid)
-	end
+  def student
+    Student.find_by_emplid(self.emplid)
+  end
 
-	def other_owner
-		return Advisor.find_by_emplid(self.emplid) if owner.kind_of?(Student)
-		return Student.find_by_emplid(self.emplid) if owner.kind_of?(Advisor)
-	end
+  def other_owner
+    return Advisor.find_by_emplid(self.emplid) if owner.kind_of?(Student)
+    return Student.find_by_emplid(self.emplid) if owner.kind_of?(Advisor)
+  end
 
   def switchable?
     if other_owner.present? && other_owner.kind_of?(Student)
@@ -57,18 +57,18 @@ class User < ActiveRecord::Base
     end
   end
 
-	def alternate_owner!
-		update_attribute(:owner, other_owner) if switchable?
-	end
+  def alternate_owner!
+    update_attribute(:owner, other_owner) if switchable?
+  end
 
-	private
+  private
 
-	def find_owner
-		if (advisor = Advisor.find_by_emplid(self.emplid)) and authorized?
+  def find_owner
+    if (advisor = Advisor.find_by_emplid(self.emplid)) and authorized?
       update_attribute(:owner, advisor)
-		elsif student = Student.find_by_emplid(self.emplid)
-			update_attribute(:owner, student)
-		end
-	end
+    elsif student = Student.find_by_emplid(self.emplid)
+      update_attribute(:owner, student)
+    end
+  end
 
 end
